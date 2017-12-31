85928bf5d93835607ebfaa3f2fe58848ca9119bc
Fejcvk <tomaszfejcak@icloud.com>
2017-12-31 14:07:36 +0100

    Task 1 Finished, Histogram Streching implemented

--- BiometriaLab2/Form1.Designer.cs
+++ BiometriaLab2/Form1.Designer.cs
@@ -28,20 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.rChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.gChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.bChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.strechingButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bChart)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(506, 514);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // rChart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.rChart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.rChart.Legends.Add(legend1);
+            this.rChart.Location = new System.Drawing.Point(777, 12);
+            this.rChart.Name = "rChart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.rChart.Series.Add(series1);
+            this.rChart.Size = new System.Drawing.Size(408, 234);
+            this.rChart.TabIndex = 1;
+            this.rChart.Text = "chart1";
+            // 
+            // gChart
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.gChart.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.gChart.Legends.Add(legend2);
+            this.gChart.Location = new System.Drawing.Point(777, 265);
+            this.gChart.Name = "gChart";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.gChart.Series.Add(series2);
+            this.gChart.Size = new System.Drawing.Size(408, 234);
+            this.gChart.TabIndex = 2;
+            this.gChart.Text = "chart2";
+            // 
+            // bChart
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.bChart.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.bChart.Legends.Add(legend3);
+            this.bChart.Location = new System.Drawing.Point(777, 512);
+            this.bChart.Name = "bChart";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.bChart.Series.Add(series3);
+            this.bChart.Size = new System.Drawing.Size(408, 234);
+            this.bChart.TabIndex = 3;
+            this.bChart.Text = "chart3";
+            // 
+            // strechingButton
+            // 
+            this.strechingButton.Location = new System.Drawing.Point(12, 532);
+            this.strechingButton.Name = "strechingButton";
+            this.strechingButton.Size = new System.Drawing.Size(75, 23);
+            this.strechingButton.TabIndex = 4;
+            this.strechingButton.Text = "Streching";
+            this.strechingButton.UseVisualStyleBackColor = true;
+            this.strechingButton.Click += new System.EventHandler(this.strechingButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(94, 531);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 5;
+            this.resetButton.Text = "Reset Image";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1260, 758);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.strechingButton);
+            this.Controls.Add(this.bChart);
+            this.Controls.Add(this.gChart);
+            this.Controls.Add(this.rChart);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bChart)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart rChart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart gChart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart bChart;
+        private System.Windows.Forms.Button strechingButton;
+        private System.Windows.Forms.Button resetButton;
     }
 }
 
--- BiometriaLab2/Form1.cs
+++ BiometriaLab2/Form1.cs
@@ -16,6 +16,91 @@ namespace BiometriaLab2
         {
             InitializeComponent();
         }
-        private void histogramStreching()
+        private void histogramStreching(Bitmap bitmap)
+        {
+            List<int> minVals = minAndMxChannelValue(bitmap);
+            var minR = minVals.ElementAt(0);
+            var minG = minVals.ElementAt(1);
+            var minB = minVals.ElementAt(2);
+            var maxR = minVals.ElementAt(3);
+            var maxG = minVals.ElementAt(4);
+            var maxB = minVals.ElementAt(5);
+            var height = bitmap.Height;
+            var width = bitmap.Width;
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    var currRGB = bitmap.GetPixel(x, y);
+                    var currR = currRGB.R;
+                    var currG = currRGB.G;
+                    var currB = currRGB.B;
+                    var newR = lutTransformation(currR, maxR, minR);
+                    var newG = lutTransformation(currG, maxG, minG);
+                    var newB = lutTransformation(currB, maxB, minB);
+                    var newColour = Color.FromArgb(currRGB.A, (int)newR, (int)newG, (int)newB);
+                    bitmap.SetPixel(x, y, newColour);
+                }
+            }
+            pictureBox1.Image = bitmap;
+        }
+        private List<int> minAndMxChannelValue(Bitmap bitmap)
+        {
+            var height = bitmap.Height;
+            var width = bitmap.Width;
+            var smallestRValue = 255;
+            var smallestGValue = 255;
+            var smallestBValue = 255;
+            var biggestRval = 0;
+            var biggestGval = 0;
+            var biggestBval = 0;
+            var minMaxChannelVal = new List<int>();
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    var currRGB = bitmap.GetPixel(x, y);
+                    var R = currRGB.R;
+                    var G = currRGB.G;
+                    var B = currRGB.B;
+                    if (R < smallestRValue)
+                        smallestRValue = R;
+                    if (B < smallestBValue)
+                        smallestBValue = B;
+                    if (G < smallestGValue)
+                        smallestGValue = G;
+                    if (R > biggestBval)
+                        biggestRval = R;
+                    if (G > biggestGval)
+                        biggestGval = G;
+                    if (B > biggestBval)
+                        biggestBval = B;
+                }
+            }
+            minMaxChannelVal.Add(smallestRValue);
+            minMaxChannelVal.Add(smallestGValue);
+            minMaxChannelVal.Add(smallestBValue);
+            minMaxChannelVal.Add(biggestBval);
+            minMaxChannelVal.Add(biggestGval);
+            minMaxChannelVal.Add(biggestBval);
+            return minMaxChannelVal;
+        }
+        private int lutTransformation(int currVal, int maxVal, int minVal)
+        {
+            int transformedVal = 255 / (maxVal - minVal) * (currVal - minVal);
+            return transformedVal;
+        }
+
+        private void strechingButton_Click(object sender, EventArgs e)
+        {
+            Bitmap bitmap = new Bitmap(BiometriaLab2.Properties.Resources.Lenna);
+            histogramStreching(bitmap);
+
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = BiometriaLab2.Properties.Resources.Lenna;
+        }
     }
 }
